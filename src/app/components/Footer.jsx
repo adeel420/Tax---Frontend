84db2b57cf633e7f4cf69bbf30a61f9b384c842b
Adeel Imran <143376329+adeel420@users.noreply.github.com>
2025-10-06 13:19:50 +0500

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -67,19 +67,30 @@ export default function Footer() {
               </a>
               <p>Lakeville, Indiana</p>
               <div className="flex space-x-4 pt-4">
-                {["twitter", "facebook"].map((icon, i) => (
+                {[
+                  {
+                    name: "instagram",
+                    url: "https://www.instagram.com/hermanomsa?igsh=Z29nY29ldzk2azg1&utm_source=qr",
+                  },
+                  {
+                    name: "facebook",
+                    url: "https://www.facebook.com/share/16RZZbminm/?mibextid=wwXIfr",
+                  },
+                ].map(({ name, url }, i) => (
                   <a
                     key={i}
-                    href="#"
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="w-9 h-9 bg-slate-800 rounded-lg flex items-center justify-center hover:bg-blue-600 transition-colors"
                   >
-                    {icon === "twitter" ? (
+                    {name === "instagram" ? (
                       <svg
                         className="w-4 h-4"
                         fill="currentColor"
                         viewBox="0 0 24 24"
                       >
-                        <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+                        <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm4.25 3.25a5.25 5.25 0 1 1 0 10.5 5.25 5.25 0 0 1 0-10.5zm0 1.5a3.75 3.75 0 1 0 0 7.5 3.75 3.75 0 0 0 0-7.5zm5.5-.75a.75.75 0 1 1 0 1.5.75.75 0 0 1 0-1.5z" />
                       </svg>
                     ) : (
                       <svg
