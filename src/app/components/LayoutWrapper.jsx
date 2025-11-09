b0c8dec3f93ab9c8cc0c5d649e5ee4640a13e261
Adeel Imran <143376329+adeel420@users.noreply.github.com>
2025-11-09 18:54:57 +0500

    Update LayoutWrapper

--- src/app/components/LayoutWrapper.jsx
+++ src/app/components/LayoutWrapper.jsx
@@ -1,28 +1,28 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import Header from "./Header";
-import Footer from "./Footer";
+// import { usePathname } from "next/navigation";
+// import Header from "./Header";
+// import Footer from "./Footer";
 import BackToTop from "./BackToTop";
 
 export default function LayoutWrapper({ children }) {
-  const pathname = usePathname();
-  const hideHeaderPaths = [
-    "/admin-dashboard",
-    "/login",
-    "/signup",
-    "/forget-password",
-    "/verify-email",
-    "/reset-password",
-    "/user-dashboard",
-  ];
-  const hideHeaderFooter = hideHeaderPaths.includes(pathname);
+  // const pathname = usePathname();
+  // const hideHeaderPaths = [
+  //   "/admin-dashboard",
+  //   "/login",
+  //   "/signup",
+  //   "/forget-password",
+  //   "/verify-email",
+  //   "/reset-password",
+  //   "/user-dashboard",
+  // ];
+  // const hideHeaderFooter = hideHeaderPaths.includes(pathname);
 
   return (
     <>
-      /* {!hideHeaderFooter && <Header />} */
+       
       {children}
-      /* {!hideHeaderFooter && <Footer />} */
+      
       <BackToTop />
     </>
   );
