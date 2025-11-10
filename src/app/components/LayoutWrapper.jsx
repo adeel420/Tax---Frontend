3eda76bdd4f0ec59bb5111642487944379f7fa5b
kaifdev01 <kaiftanveer943@gmail.com>
2025-11-10 21:06:08 +0500

    Update LayoutWrapper.jsx

--- src/app/components/LayoutWrapper.jsx
+++ src/app/components/LayoutWrapper.jsx
@@ -6,23 +6,13 @@ import Footer from "./Footer";
 import BackToTop from "./BackToTop";
 
 export default function LayoutWrapper({ children }) {
-  const pathname = usePathname();
-  const hideHeaderPaths = [
-    "/admin-dashboard",
-    "/login",
-    "/signup",
-    "/forget-password",
-    "/verify-email",
-    "/reset-password",
-    "/user-dashboard",
-  ];
-  const hideHeaderFooter = hideHeaderPaths.includes(pathname);
+  );
 
   return (
     <>
-      {/* {!hideHeaderFooter && <Header />} */}
+     <Header />
       {children}
-      {/* {!hideHeaderFooter && <Footer />} */}
+    && <Footer />
       <BackToTop />
     </>
   );
